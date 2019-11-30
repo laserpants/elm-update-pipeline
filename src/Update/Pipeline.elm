c0b7177fd964a41114e8558654b539667ca0e627
Johannes Hildén <hildenjohannes@gmail.com>
2019-12-01 00:51:22 +0100

    Improve docblocks

--- src/Update/Pipeline.elm
+++ src/Update/Pipeline.elm
@@ -1,37 +1,57 @@
 module Update.Pipeline exposing
-    ( pure, map, addCmd, mapCmd, join
+    ( save, map, addCmd, mapCmd, join
     , andThen, sequence, when, andIf, kleisli
     , map2, map3, map4, map5, map6, map7, andMap
     , using, with
     , andAddCmd, andUsing, andWith
     )
 
-{-|
+{-| This module provides an interface for sequential composition of updates in the style of pipelines,
+where _monadic_ functions of the type `a -> ( b, Cmd msg )` are chained together using
+the pipes operator. For example;
+
+    update msg model =
+        case msg of
+            SomeMsg someMsg ->
+                save model
+                    |> andThen ??
+                    |> andThen (setAllDone True)
+                    |> andAddCmd someCmd
+
+It also lends itself naturally to, so called, _pointfree_ style of code:
+
+    update msg =
+        case msg of
+            SomeMsg someMsg ->
+                save
+                    >> andThen ??
+                    >> andThen (setAllDone True)
+                    >> andAddCmd someCmd
 
 
-## Basics
+# Basics
 
-@docs pure, map, addCmd, mapCmd, join
+@docs save, map, addCmd, mapCmd, join
 
 
-## Chaining Updates
+# Chaining Updates
 
 @docs andThen, sequence, when, andIf, kleisli
 
 
-## Applicative Interface
+# Applicative Interface
 
 These functions address the need to map over functions with more than one parameter.
 
 @docs map2, map3, map4, map5, map6, map7, andMap
 
 
-## Pointfree Helpers
+# Pointfree Helpers
 
 @docs using, with
 
 
-## Other Shortcuts
+# Other Shortcuts
 
 @docs andAddCmd, andUsing, andWith
 
@@ -40,23 +60,21 @@ These functions address the need to map over functions with more than one parame
 
 {-| Inject a value into a `( model, Cmd msg )` pair without adding any commands.
 
-    pure model =
+    save model =
         ( model, Cmd.none )
+
 -}
-pure : a -> ( a, Cmd msg )
-pure model =
+save : a -> ( a, Cmd msg )
+save model =
     ( model, Cmd.none )
 
 
-{-| Apply a function to the first component of a `( model, Cmd msg )` pair.
-
-If partially applied, we can also think of this as taking a function `a -> b` and “lifting” it into one of type `( a, Cmd msg ) -> ( b, Cmd msg )`.
+{-| Apply a function to the model (i.e. first component) of a `( model, Cmd msg )` pair.
+When partially applied, we can also think of this as taking a function `a -> b` and “lifting” it into one of type `( a, Cmd msg ) -> ( b, Cmd msg )`.
 -}
 map : (a -> b) -> ( a, Cmd msg ) -> ( b, Cmd msg )
-map fun ( model, cmd ) =
-    ( fun model
-    , cmd
-    )
+map =
+    Tuple.mapFirst
 
 
 ap :
@@ -158,22 +176,32 @@ map7 f a b c d e =
 
 {-| Trying to map over a function `number -> number -> number`,
 
-    map (+) (pure 4)
+    map (+) (save 4)
 
-we end up with a result of type `( (number -> number), Cmd msg )`. 
+we end up with a result of type `( (number -> number), Cmd msg )`.
 
+    type alias Model =
+        { xxx : a
+        , yyy : a
+        , zzz : a
+        }
 
+    init : Flags -> ( Model, Cmd Msg )
+    init flags =
+        save Model
+            |> andMap xxx
+            |> andMap yyy
+            |> andMap zzz
 
-If not sooner, you’ll need this when you want to map functions of more than seven parameters.
+See also [`map2`](#map2), [`map3`](#map3), etc. If not sooner, you’ll need this function when you want to `mapN` for _N > 7_.
 
-See also [`map2`](#map2), [`map3`](#map3), etc.
 -}
 andMap : ( a, Cmd msg ) -> ( a -> b, Cmd msg ) -> ( b, Cmd msg )
 andMap a b =
     ap b a
 
 
-{-|
+{-| `andThen` is defined as `\f -> join << map f`.
 -}
 join : ( ( a, Cmd msg ), Cmd msg ) -> ( a, Cmd msg )
 join ( ( model, cmd1 ), cmd2 ) =
@@ -182,15 +210,13 @@ join ( ( model, cmd1 ), cmd2 ) =
     )
 
 
-{-|
--}
+{-| -}
 andThen : (b -> ( a, Cmd msg )) -> ( b, Cmd msg ) -> ( a, Cmd msg )
-andThen next =
-    join << map next
+andThen f =
+    join << map f
 
 
-{-|
--}
+{-| -}
 kleisli :
     (b -> ( c, Cmd msg ))
     -> (a -> ( b, Cmd msg ))
@@ -200,57 +226,63 @@ kleisli f g =
     andThen f << g
 
 
-{-|
--}
+{-| -}
 sequence : List (a -> ( a, Cmd msg )) -> a -> ( a, Cmd msg )
 sequence list model =
-    List.foldl andThen (pure model) list
+    List.foldl andThen (save model) list
 
 
-{-| Create a `( model, Cmd msg)` pair from the two arguments. For example;
+{-| Create a `( model, Cmd msg)` pair from the two arguments.
 
-    update msg model =
-        case msg of
-            SomeMsg someMsg ->
-                model
-                    |> addCmd myCmd
-                    |> andThen (setStatus Done)
+This function is simply defined as `addCmd cmd model = ( model, cmd )`, but it can still be pretty useful.
+For example, in idiomatic use, one could write
+
+    { model | power = 100 }
+        |> addCmd someCmd
+        |> andThen (setStatus Done)
+
+… instead of
+
+    ( { model | power = 100 }, someCmd )
+        |> andThen (setStatus Done)
 
 See also [`andAddCmd`](#andAddCmd).
+
 -}
 addCmd : Cmd msg -> a -> ( a, Cmd msg )
 addCmd cmd model =
     ( model, cmd )
 
 
-{-|
+{-| Transform the message produced by `Cmd` part of a `( model, Cmd msg )` pair.
 -}
 mapCmd : (msg1 -> msg2) -> ( a, Cmd msg1 ) -> ( a, Cmd msg2 )
-mapCmd fun ( model, cmd ) =
-    ( model
-    , Cmd.map fun cmd
-    )
+mapCmd =
+    Tuple.mapSecond << Cmd.map
 
 
 {-| Shortcut for `andThen << addCmd`
+
+Example use:
+
+    model
+        |> save
+        |> andAddCmd someCmd
+
 -}
 andAddCmd : Cmd msg -> ( a, Cmd msg ) -> ( a, Cmd msg )
 andAddCmd =
     andThen << addCmd
 
 
-{-|
-
-See also [`andWith`](#andWith).
+{-| See also [`andWith`](#andWith).
 -}
 with : (a -> b) -> (b -> a -> c) -> a -> c
 with view fun =
     using (fun << view)
 
 
-{-|
-
-See also [`andUsing`](#andUsing).
+{-| See also [`andUsing`](#andUsing).
 -}
 using : (a -> a -> b) -> a -> b
 using fun model =
@@ -260,6 +292,7 @@ using fun model =
 {-| Run an update if the given condition is `True`, otherwise do nothing.
 
 See also [`andIf`](#andIf).
+
 -}
 when :
     Bool
@@ -271,7 +304,7 @@ when cond fun =
         fun
 
     else
-        pure
+        save
 
 
 {-| Shortcut for `\view -> andThen << with view`
